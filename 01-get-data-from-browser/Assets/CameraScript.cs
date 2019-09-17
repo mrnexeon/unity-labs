5e52f92b3fbc3fbd1e1251757e337b2e7889b75f
mrnexeon <the.nexeon@gmail.com>
2019-09-17 23:56:45 +0300

    First lab

---
+++ 01-get-data-from-browser/Assets/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public TextMesh textMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.ExternalEval("OnUnityReady()");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setText(string text) {
+        textMesh.text = text;
+    }
+}
